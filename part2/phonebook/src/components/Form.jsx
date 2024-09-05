6abec0f500f2608f35055c70eb5ed42026e6ac5b
CalebBravo <calebphone72@gmail.com>
2024-09-05 14:44:33 -0300

    2.7: La Agenda Telefónica Paso 2

--- part2/phonebook/src/components/Form.jsx
+++ part2/phonebook/src/components/Form.jsx
@@ -3,14 +3,24 @@ import { useState } from 'react';
 const Form = ({ persons, setPersons }) => {
 
     const [newName, setNewName] = useState('');
-  const inputHandler = (e) => setNewName(e.target.value);
+    const inputHandler = (e) => setNewName(e.target.value);
 
     const addPerson = (e) => {
-        e.preventDefault();
-        e.target[0].value = "";
-        setPersons([...persons, { name: newName}]);
-        setNewName("");
-      }
+
+        e.preventDefault(); // Evita que se recaaague la pagina
+        
+        if(persons.filter((element) => element.name === newName).length) {
+
+            newName === "Arto Hellas" ? alert(`Arto Hellas is already added to phonebook`)
+            : alert(`${newName} is already added to phonebook`);
+
+        } else {
+            setPersons([...persons, { name: newName }]);
+        }
+
+        setNewName(""); // Limpia el estado del manejador de eventos
+        e.target[0].value = ""; // Limpia la entrade de texto
+    }
 
     return (
         <form onSubmit={addPerson}>
