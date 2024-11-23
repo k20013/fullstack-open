b6b745d1674e1f8ba9a757799b6edf74c1668af6
CalebBravo <calebphone72@gmail.com>
2024-11-23 13:59:18 -0300

    4.5*: Funciones Auxiliares y Pruebas Unitarias, paso 3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -12,7 +12,78 @@ const totalLikes = (blogs) => {
     return total;
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 1) {
+        return blogs[0];
+    } else if (blogs.length > 1) {
+
+        let favorite = blogs[0];
+        for (let i = 1; i < blogs.length; i++) {
+            favorite.likes < blogs[i].likes
+                ? favorite = blogs[i]
+                : null;
+        }
+        return favorite;
+    } else {
+        return {};
+    }
+}
+
+// const blogs = [
+//     {
+//         _id: "5a422a851b54a676234d17f7",
+//         title: "React patterns",
+//         author: "Michael Chan",
+//         url: "https://reactpatterns.com/",
+//         likes: 7,
+//         __v: 0
+//     },
+//     {
+//         _id: "5a422aa71b54a676234d17f8",
+//         title: "Go To Statement Considered Harmful",
+//         author: "Edsger W. Dijkstra",
+//         url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+//         likes: 5,
+//         __v: 0
+//     },
+//     {
+//         _id: "5a422b3a1b54a676234d17f9",
+//         title: "Canonical string reduction",
+//         author: "Edsger W. Dijkstra",
+//         url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+//         likes: 12,
+//         __v: 0
+//     },
+//     {
+//         _id: "5a422b891b54a676234d17fa",
+//         title: "First class tests",
+//         author: "Robert C. Martin",
+//         url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+//         likes: 10,
+//         __v: 0
+//     },
+//     {
+//         _id: "5a422ba71b54a676234d17fb",
+//         title: "TDD harms architecture",
+//         author: "Robert C. Martin",
+//         url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+//         likes: 0,
+//         __v: 0
+//     },
+//     {
+//         _id: "5a422bc61b54a676234d17fc",
+//         title: "Type wars",
+//         author: "Robert C. Martin",
+//         url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+//         likes: 2,
+//         __v: 0
+//     }
+// ];
+const blogs = [];
+console.log(favoriteBlog(blogs))
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
